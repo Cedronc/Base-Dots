e90a46e8499b9948c303911d0929968a7a1c389d
Cedric Martel <cedric@Cedrics-MacBook-Pro.local>
2025-10-15 23:47:10 +0200

    golf & spell added

--- nvim/.config/nvim/lua/custom/configs/opts.lua
+++ nvim/.config/nvim/lua/custom/configs/opts.lua
@@ -1,4 +1,5 @@
 -- vim.cmd 'set path+=**'
+vim.opt.spelllang = { "en", "nl" } -- Set languages
 
 -- needs to be here to work with import inside init.lua
 return {}
---
+++ nvim/.config/nvim/lua/custom/configs/spell.lua
@@ -0,0 +1,36 @@
+-- Add word in a specific language
+local function add_word_to_lang(lang)
+	-- Find word under cursor
+	local word = vim.fn.expand("<cword>")
+	if word == "" then
+		print("No word under cursor.")
+		return
+	end
+
+	-- Save original language and spellfile
+	local original_lang = vim.opt.spelllang:get()
+	local original_spellfile = vim.opt.spellfile
+
+	-- Get the current language file
+	local spellfile_path = vim.fn.expand(string.format("~/Base-Dots/nvim/.config/nvim/spell/%s.utf-8.add", lang))
+
+	-- Temporarily set spelllang to only the desired language
+	vim.opt.spelllang = { lang }
+	vim.opt.spellfile = spellfile_path
+
+	-- Mark word as good and save spellfile
+	vim.cmd("silent! spellgood " .. word)
+	vim.cmd("silent! mkspell! " .. spellfile_path)
+
+	-- Restore original settings
+	vim.opt.spelllang = original_lang
+	vim.opt.spellfile = original_spellfile
+
+	print("Added '" .. word .. "' to " .. lang)
+end
+
+vim.keymap.set('n', "<leader>z", "", { desc = "Spelling" })
+vim.keymap.set('n', "<leader>zn", function() add_word_to_lang("nl") end, { desc = "Add word to Dutch" })
+vim.keymap.set('n', "<leader>ze", function() add_word_to_lang("en") end, { desc = "Add word to English" })
+
+return {}
