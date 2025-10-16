a0ada9d264041c260e27d4668a21de971ac601f4
Cedric Martel <cedric@Cedrics-MacBook-Pro.local>
2025-10-16 19:46:04 +0200

    feat: keymap for vertical term

--- nvim/.config/nvim/lua/custom/configs/random-binds.lua
+++ nvim/.config/nvim/lua/custom/configs/random-binds.lua
@@ -1,4 +1,6 @@
 vim.keymap.set('n', '<leader>w', ':update<CR>')
 vim.keymap.set('n', '<leader>q', ':q<CR>')
 
+vim.keymap.set('n', '<C-s>vt', ':vertical split<CR> :vertical resize -25<CR> :term<CR>')
+
 return {}
