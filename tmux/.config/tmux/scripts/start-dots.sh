72636fc57ac6abb265ccbfaecc8232d1a2cad560
Cedric Martel <cedric@Cedrics-MacBook-Pro.local>
2025-10-23 11:06:00 +0200

    add tmux start session scripts

---
+++ tmux/.config/tmux/scripts/start-dots.sh
@@ -0,0 +1,16 @@
+#!/bin/zsh
+
+SESSIONNAME="Dots"
+tmux has-session -t $SESSIONNAME &> /dev/null
+
+if [ $? != 0 ] 
+ then
+    tmux new-session -s $SESSIONNAME -n lazygit -c ~/Base-Dots/.
+    tmux send-keys -t $SESSIONNAME "lazygit" C-m 
+    tmux new-window -n nvim -c ~/Base-Dots/.
+    tmux send-keys -t $SESSIONNAME "nvim ." C-m
+    tmux new-window -c ~/Base-Dots/. -n stow
+fi
+
+tmux attach -t $SESSIONNAME
+
--- tmux/.config/tmux/scripts/start-vub.sh
+++ tmux/.config/tmux/scripts/start-vub.sh
@@ -5,12 +5,11 @@ tmux has-session -t $SESSIONNAME &> /dev/null
 
 if [ $? != 0 ] 
  then
-    tmux new-session -s $SESSIONNAME -n lazygit -d -c "~/Documents/git/VUB/."
+    tmux new-session -s $SESSIONNAME -n lazygit -c ~/Documents/git/VUB/.
     tmux send-keys -t $SESSIONNAME "lazygit" C-m 
-    tmux new-window -n nvim
-    tmux send-keys -t nvim "nvim ." C-m 
-    tmux new-window -c "~/Documents/MyVault/." -n MyVault
-    tmux send-keys "nvim ." C-m 
+    tmux new-window -n nvim -c ~/Documents/git/VUB/.
+    tmux new-window -c ~/Documents/MyVault/. -n MyVault
+    tmux select-window -t nvim
 fi
 
 tmux attach -t $SESSIONNAME
