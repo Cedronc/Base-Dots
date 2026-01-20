e4baf4e7b34460696ef45d1bc4d4c7b59443865e
Cedric Martel <cedricmrtl@pm.me>
2026-01-20 17:24:01 +0100

    small keybind / behaviour changes

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -356,6 +356,7 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sc', builtin.commands, { desc = '[S]earch [C]ommands' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
       vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+      vim.keymap.set('n', '<leader>st', ":Telescope todo-comments<CR>", { desc = '[S]earch [T]odo comments' })
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
       vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
       vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
