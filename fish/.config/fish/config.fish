b656d40668f66f3487b58dd7105cecaee3c76426
Cedric Martel <cedricmrtl@pm.me>
2026-07-22 14:09:25 +0200

    added fish

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,22 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end
+
+set -gx EDITOR nvim
+
+function lf
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	command yazi $argv --cwd-file="$tmp"
+	if read -z cwd < "$tmp"; and [ "$cwd" != "$PWD" ]; and test -d "$cwd"
+		builtin cd -- "$cwd"
+	end
+	command rm -f -- "$tmp"
+end
+
+function lg
+  command lazygit
+end
