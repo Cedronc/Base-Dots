e41b10f66a2697a6d8ba275ecfe17ab8839a70ea
Cedric Martel <cedricmrtl@pm.me>
2026-08-26 14:28:11 +0200

    updated telescope view

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -324,19 +324,19 @@ require('lazy').setup({
 
       -- [[ Configure Telescope ]]
       -- See `:help telescope` and `:help telescope.setup()`
-      require('telescope').setup {
-        defaults = {
-          layout_strategy = 'horizontal',
-          layout_config = {
-            horizontal = {
-              prompt_position = 'bottom',
-              width = { padding = 0 },
-              height = { padding = 0 },
-              preview_width = 0.5,
-            },
-          },
-          border = true,
-        },
+       require('telescope').setup {
+         defaults = {
+           layout_strategy = 'horizontal',
+           layout_config = {
+             horizontal = {
+               prompt_position = 'bottom',
+               width = 0.8,
+               height = 0.8,
+               preview_width = 0.5,
+             },
+           },
+           border = true,
+         },
         picker = { 'fzf' },
         extensions = {
           ['ui-select'] = {
